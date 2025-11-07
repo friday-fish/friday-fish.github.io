f2071041a4e81fe90ef5f963df04ecaaea910c9d
Douwe Hoekstra <dev@dhoekstra.xyz>
2025-11-07 13:27:08 +0100

    Add timezone to ical file

--- _plugins/calendar.rb
+++ _plugins/calendar.rb
@@ -1,5 +1,6 @@
 require 'jekyll'
 require 'icalendar'
+require 'icalendar/tzinfo'
 require 'time'
 require 'tzinfo'
 require 'securerandom'
@@ -7,7 +8,13 @@ require 'securerandom'
 module Jekyll
   class ICalendarGenerator < Jekyll::Generator
     def make_calendar(site, config)
+      
       cal = Icalendar::Calendar.new
+
+      tzid = "Europe/Amsterdam"
+      tz = TZInfo::Timezone.get(tzid)
+      timezone = tz.ical_timezone Time.now
+      cal.add_timezone timezone
 
       upcoming_talks = site.collections['talks'].docs.select do |talk|
         date = talk.data["talk_date"]
@@ -26,10 +33,10 @@ module Jekyll
           zone = TZInfo::Timezone.get("Europe/Amsterdam")
           start_hour, start_min = talk.data["start_time"].split(":").map(&:to_i)
           end_hour, end_min = talk.data["end_time"].split(":").map(&:to_i)
-          start_time = zone.local_time(date.year, date.month, date.day, start_hour, start_min, 0)
-          end_time = zone.local_time(date.year, date.month, date.day, end_hour, end_min, 0)
-          e.dtstart = start_time
-          e.dtend = end_time
+          start_time = Time.new(date.year, date.month, date.day, start_hour, start_min, 0)
+          end_time = Time.new(date.year, date.month, date.day, end_hour, end_min, 0)
+          e.dtstart = Icalendar::Values::DateTime.new start_time, 'tzid' => tzid
+          e.dtend = Icalendar::Values::DateTime.new end_time, 'tzid' => tzid
           e.summary = talk.data['speaker'] + ": " + talk.data['title']
           e.description = talk.data['abstract']
           e.location = talk.data['location']
